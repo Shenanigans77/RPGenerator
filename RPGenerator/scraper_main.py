a799920be13334e3cf65efe452fb896277351c6a
Sean Shanahan <seanmodg@gmail.com>
2019-04-30 14:43:22 -0400

    Added lxml parser to scraper.

--- RPGenerator/scraper_main.py
+++ RPGenerator/scraper_main.py
@@ -1,7 +1,9 @@
 from bs4 import BeautifulSoup
+from lxml import html
 import requests
 import re
 import unittest
+import json
 
 
 # search = input("Search For:")
@@ -10,15 +12,22 @@ import unittest
 # Get monster entries from d20 srd
 r = requests.get("http://www.d20srd.org/indexes/monsters.htm")
 
-soup = BeautifulSoup(r.text, "html.parser")
+soup = BeautifulSoup(r.text, "lxml")
 
 # search soup for monster page hyperlinks
 for result in soup.find_all(href=re.compile("monsters")):
-    # print("{}: {}".format(result.string, result.attrs))
+    print("{}: {}".format(result.string, result.attrs))
     url = str("http://www.d20srd.org" + result.attrs["href"])
-    # print('{}'.format(url))
+    print('{}'.format(url))
     # get stat tables for each monster entry
     entry = requests.get(url=url)
-    entry_soup = BeautifulSoup(entry.text, "html.parser")
+    
+    #print("{}".format(entry))
+    entry_soup = BeautifulSoup(entry.text, "lxml")
+    
+    print("{}".format(entry_soup))
 
 # TODO
+# http://www.d20srd.org/indexes/monsters.htm
+# http://www.d20srd.org/srd/monsters/aboleth.htm
+# http://www.d20srd.org/srd/monsters/etherealFilcher.htm
